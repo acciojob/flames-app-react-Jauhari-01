bb24beb4eb49a95e4408dd3e3b7569514f583e4d
Shubham Jauhari <90859295+Jauhari-01@users.noreply.github.com>
2024-03-29 17:29:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
-class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+import React, {Component, useState} from “react”;
+import ‘…/styles/App.css’;
+const App = () => {
+const[name1,setName1] = useState(“”);
+const[name2,setName2] = useState(“”);
+const OnChange1 = (event) => {
+setName1(event.target.value);
 }
+const OnChange2 = (event) => {
+setName2(event.target.value);
+}
+const BtnClicked = () => {
+let ans = name1.(name2);
+console.log(ans);
+}
+return(
+    <div id="main">
+       {/* Do not remove the main div */}
+       <input placeholder="Enter first name" onChange={OnChange1} />
+       <input placeholder="Enter second name" onChange={OnChange2} />
+       <button onClick={BtnClicked}>Calculate Relationship future</button>
+       <button>Clear</button>
+    </div>
+)
 
-
+}
 export default App;
+
